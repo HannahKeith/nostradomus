7cf2d63214506da1be4f842c2b015efb31540be2
Hannah Keith <hannahkeith@hannahs-air.attlocal.net>
2020-05-15 17:42:49 -0400

    built main API framework

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
 console.log('We have liftoff!');
+
+
+$(() => {
+
+
+$('#start button').on('click',(event) => {
+
+
+$.ajax({
+  url: 'https://eonet.sci.gsfc.nasa.gov/api/v2.1/events',
+  type: 'GET',
+  data: {
+    '$limit': 5
+  }
+
+
+})
+  .done (
+    (data) => {
+      let $body = $('body')
+      for (let i = 0; i < data.length; i++){
+        $seeEvent = $('<h2>')
+          .text(data[i].events.title)
+          .appendTo($body)
+          console.log('test');
+      }
+    }
+
+  )
+
+
+  //ends event listener
+})
+
+
+
+
+
+
+
+
+  //Ends DOR
+})
