683f2482a89fc98d5e75ab0becc594cae4f118c2
Hannah Keith <hannahkeith@hannahs-air.attlocal.net>
2020-05-17 15:04:03 -0400

    basic styling done

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
 // console.log('We have liftoff!');
 
 
-// $(() => {
-
-
-
-
-
 //After my syntax didn't work over and over, I followed NASA's how to guide on using thier APOD API and came up with the following
 
 
@@ -26,7 +20,7 @@
         // console.log(response);
         //This part I figured out using jquery documentation on creating an img element
       let pic1 = response.hdurl;
-        console.log(pic1);
+        // console.log(pic1);
         $seePOD = $('<img>')
          .appendTo('.ball')
          .attr({
@@ -72,27 +66,69 @@
 // })
 
 
-
+// const randomNumGenerator(predictions) {
+//   return Math.floor(Math.random() * predictions.length)
+// }
 
 //Starts DOR
 $(() => {
 
 
 
-let modal = $('#simpleModal')
-let modalBtn = $('#modalBtn')
-let closeBtn = $('.closeBtn')
+  let predictions = [
+    'the young lion will overcome the older one', 'two wounds made one, then he dies a cruel death', 'on the field of combat in a single battle', 'the ancient lady will fall from her high place', 'burnt through lightening of twenty threes the six', 'chants and demands', 'these will in the future by headless idiots', 'be received as divine prayers', 'this is when the moon completes her great cycle', 'his fame will increase towards the realm of the east'
+  ];
+
+let modal = $('#simpleModal');
+let modalBtn = $('#quoteBtn');
+let closeBtn = $('.closeBtn');
+
+
 
 modalBtn.on('click', (event) => {
+  // randomGenerator() {
+  //   return Math.floor(Math.random() * predictions.length)
+  // };
+  // generatePrediction() {
+  //   console.log(`Nostra-DOM-us says ${this.predictions[this.randomNumGenerator(this.predictions)]} + ${this.predictions[this.randomNumGenerator(this.predictions)]} + ${this.predictions[this.randomNumGenerator(this.predictions)]}`);
+  // }
    $('.modal').css('display','block')
-   // console.log('test');
-})
+   $oracle = $('<p>')
+    .text()
 
+});
 
 closeBtn.on('click', (event) => {
     // console.log('test');
   modal.css('display','none')
+
+});
+
+$('#quoteBtn').on('click', () => {
+
+  // randomGenerator() {
+  //   return Math.floor(Math.random() * predictions.length)
+  // };
+
+  // generatePrediction() {
+  //   console.log(`Nostra-DOM-us says ${this.predictions[this.randomNumGenerator(this.predictions)]} + ${this.predictions[this.randomNumGenerator(this.predictions)]} + ${this.predictions[this.randomNumGenerator(this.predictions)]}`);
+  // }
 })
+
+
+
+
+
+//
+
+
+
+
+// const randomNumGenerator(predictions) {
+//   return Math.floor(Math.random() * predictions.length)
+// }
+//
+
 
 
 
